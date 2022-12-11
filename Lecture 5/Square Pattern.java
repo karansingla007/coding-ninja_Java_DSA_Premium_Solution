f0b6fa88f95b65f341166e795394a8e67a822a86
Kara Singla <karansingla120@gmail.com>
2022-12-11 13:20:17 -0500

    Lecture 5 Pattern 1 codes

---
+++ Lecture 5/Square Pattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        for(int i=0;i<n;i++) {
+         for(int j=0;j<n;j++) {
+             System.out.print(n);
+         }
+            System.out.println();
+        }
+		
+	}
+
+}
---
+++ Lecture 5/Triangle Number Pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<=i;j++) {
+            System.out.print(i + 1);
+        }   
+            System.out.println();
+        }
+		
+	}
+
+}
